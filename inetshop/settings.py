7fe0009105c56c52074c923ffb385d7708e2d4f8
Айтал Емельянов <aytem95@gmail.com>
2023-10-09 00:32:02 +0600

    fixset

--- inetshop/settings.py
+++ inetshop/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Add any additional directories if needed
-]
+STATICFILES_DIRS = (
+    BASE_DIR, 'static'  # Путь к статическим файлам
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
